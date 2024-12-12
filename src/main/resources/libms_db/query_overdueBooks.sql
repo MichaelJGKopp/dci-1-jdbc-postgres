eed8e7cddea60fd98137486e2ed2fb4bebe392e8
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-12-12 15:04:38 +0100

    add overdue books query

---
+++ src/main/resources/libms_db/query_overdueBooks.sql
@@ -0,0 +1,6 @@
+SELECT b.title, m.name, age('2024-12-10', br.return_date) AS days_overdue--, br.return_date
+    FROM public.books b
+    JOIN public.borrow_records br on b.book_id = br.book_id
+    JOIN public.members m on br.member_id = m.member_id
+WHERE br.return_date < '2024-12-10'
+ORDER BY days_overdue DESC, b.title DESC
