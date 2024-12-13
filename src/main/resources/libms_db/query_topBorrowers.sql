832ad2d076300bcfedac8b96e817533c57344c3e
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-12-13 12:04:54 +0100

    add top borrowers pereira code

--- src/main/resources/libms_db/query_topBorrowers.sql
+++ src/main/resources/libms_db/query_topBorrowers.sql
@@ -5,4 +5,45 @@ FROM public.books b
          JOIN public.members m on br.member_id = m.member_id
 GROUP BY m.name
 ORDER BY borrowed_books_count DESC
-LIMIT 5
+LIMIT 5;
+
+-- Top Borrowers
+-- Write a query to identify the top 3 members who have borrowed the most books.
+-- Display their name, email, and the total number of books borrowed.
+
+SELECT members.member_id, name, COUNT(*) AS borrowed_books
+FROM borrow_records
+         JOIN members ON members.member_id = borrow_records.member_id
+GROUP BY (members.member_id)
+ORDER BY borrowed_books DESC
+LIMIT 2;
+
+
+SELECT COUNT(*)
+FROM borrow_records
+GROUP BY member_id
+ORDER BY count DESC
+LIMIT 1;
+
+SELECT members.member_id, name, COUNT(*) AS borrowed_books
+FROM borrow_records
+         JOIN members ON members.member_id = borrow_records.member_id
+GROUP BY (members.member_id)
+HAVING COUNT(*) = 2;
+
+SELECT members.member_id, name, COUNT(*) AS borrowed_books
+FROM borrow_records
+         JOIN members ON members.member_id = borrow_records.member_id
+GROUP BY (members.member_id)
+HAVING COUNT(*) IN (
+    SELECT COUNT(*)
+    FROM borrow_records
+    GROUP BY member_id
+    ORDER BY count DESC
+    LIMIT 5
+);
+
+
+SELECT *
+FROM books
+WHERE book_id IN (3, 4);
